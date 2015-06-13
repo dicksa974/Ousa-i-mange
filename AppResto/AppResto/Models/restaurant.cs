7e4a9a261b449ccfec903b6d1272de2647f91d4f
Adeline <bastide.adeline@gmail.com>
2015-06-13 14:12:29 +0400

    Fisrt push
    classe Restaurant

---
+++ AppResto/AppResto/Models/restaurant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+
+namespace AppResto.Models
+{
+    class Restaurant
+    {
+        public int Id { get; set; }
+        public String Nom { get; set; }
+        public String Adresse { get; set; }
+        public String Photo {get; set;}
+        public Geopoint position_loc { get; set; }
+        public int prix_mini { get; set; }
+        public int prix_maxi { get; set; }
+        public int phone { get; set; }
+    }
+}
